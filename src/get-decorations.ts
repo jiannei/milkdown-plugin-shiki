e27c1cf371388b2dea9ae0d266b51484106b2f69
jiannei <longjian.huang@foxmail.com>
2024-01-16 17:22:10 +0800

    pref: getDecorations

---
+++ src/get-decorations.ts
@@ -0,0 +1,50 @@
+import type { Node } from '@milkdown/prose/model'
+import type { Highlighter } from 'shikiji'
+import { Decoration, DecorationSet } from '@milkdown/prose/view'
+import { findChildren } from '@milkdown/prose'
+
+export interface FlattedNode {
+  content: string
+  color: string
+}
+
+export function getDecorations(doc: Node, name: string, highlighter: Highlighter) {
+  const decorations: Decoration[] = []
+
+  const children = findChildren(node => node.type.name === name)(doc)
+
+  children.forEach(async (block) => {
+    let from = block.pos + 1
+    const { language } = block.node.attrs
+
+    if (!language)
+      return
+
+    const nodes = highlighter.codeToThemedTokens(block.node.textContent, {
+      lang: language,
+      theme: 'night-owl',
+    }).map(token =>
+      token.map(
+        ({ content, color }) =>
+          ({
+            content,
+            color,
+          } as FlattedNode),
+      ),
+    )
+
+    nodes.forEach((block) => {
+      block.forEach((node) => {
+        const to = from + node.content.length
+        const decoration = Decoration.inline(from, to, {
+          style: `color: ${node.color}`,
+        })
+        decorations.push(decoration)
+        from = to
+      })
+      from += 1
+    })
+  })
+
+  return DecorationSet.create(doc, decorations)
+}
--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,8 @@
 import { $proseAsync } from '@milkdown/utils'
 import { findChildren } from '@milkdown/prose'
 import { Plugin, PluginKey } from '@milkdown/prose/state'
-import type { Highlighter } from 'shikiji'
 import { getHighlighter } from 'shikiji'
-import type { Node } from '@milkdown/prose/model'
-import { Decoration, DecorationSet } from '@milkdown/prose/view'
-
-export interface FlattedNode {
-  content: string
-  color: string
-}
-
-function getDecorations(doc: Node, name: string, highlighter: Highlighter) {
-  const decorations: Decoration[] = []
-
-  const children = findChildren(node => node.type.name === name)(doc)
-
-  children.forEach(async (block) => {
-    let from = block.pos + 1
-    const { language } = block.node.attrs
-
-    if (!language)
-      return
-
-    const nodes = highlighter.codeToThemedTokens(block.node.textContent, {
-      lang: language,
-      theme: 'night-owl',
-    }).map(token =>
-      token.map(
-        ({ content, color }) =>
-          ({
-            content,
-            color,
-          } as FlattedNode),
-      ),
-    )
-
-    nodes.forEach((block) => {
-      block.forEach((node) => {
-        const to = from + node.content.length
-        const decoration = Decoration.inline(from, to, {
-          style: `color: ${node.color}`,
-        })
-        decorations.push(decoration)
-        from = to
-      })
-      from += 1
-    })
-  })
-
-  return DecorationSet.create(doc, decorations)
-}
+import { getDecorations } from './get-decorations'
 
 export const shiki = $proseAsync(async () => {
   const highlighter = await getHighlighter({
